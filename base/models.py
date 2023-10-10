a053b7a55891a09c82ca79309aa02b35d0293fc7
Neilansh Chauhan <neilanshchauhan4@gmail.com>
2023-10-10 17:15:02 +0530

    Integrate Custom User Model

--- base/models.py
+++ base/models.py
@@ -1,6 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import User
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(unique=True, null=True)
+    bio = models.TextField(null=True)
+
+    avatar = models.ImageField(null=True, default="avatar.svg")
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
 
 class Topic(models.Model):
     name = models.CharField(max_length=200)
@@ -36,3 +46,4 @@ class Message(models.Model):
 
     def __str__(self):
         return self.body[0:50]
+
--- base/views.py
+++ base/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, redirect
-from .models import Room,Topic,Message
+from .models import Room,Topic,Message,User
 from django.db.models import Q
 from .forms import RoomForm,UserForm
-from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import login,logout,authenticate
 from django.contrib.auth.decorators import login_required
